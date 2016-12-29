7c6f0abb9705aa3b0730b6ae6f362fbf36542f8e
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2016-12-29 20:22:50 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import path from 'path';
+import _ from 'lodash/fp';
+
+import { walkSync } from './lib/collectInput';
+import { parseImports } from './lib/parseImports';
+import { printDot } from "./lib/printDot";
+
+const absolutePath = path.resolve(process.argv[2]);
+const files = _.flattenDeep(walkSync(absolutePath));
+const imports = parseImports(files, absolutePath);
+
+printDot(imports);
